f35a858b7fd3d35d739b94abc64e7b1571a350d2
feco <feco.boroczki@gmail.com>
2016-04-08 23:23:00 +0200

    add init js files

---
+++ src/public/js/game/init.js
@@ -0,0 +1,7 @@
+(function init() {
+    console.info('play');
+    var canvas = document.getElementById('canvas');
+    var game = new Game(canvas);
+    game.start();
+})();
+//# sourceMappingURL=init.js.map
