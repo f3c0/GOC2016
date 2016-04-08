0a540cbb9b16c96b7e93fddf06e3ab2d2f92d179
Miklós Boros <miklos.boros@doclerholding.com>
2016-04-09 01:22:53 +0200

    basic actuator

---
+++ src/public/js/game/Actuator.ts
@@ -0,0 +1,44 @@
+import Player = require('game/Player');
+
+class Actuator
+{
+    private decision;
+
+    private possibleDecisions = [
+        'accelerate',
+        'decelerate',
+        'rotateLeft',
+        'rotateRight'
+    ];
+
+    constructor(public player:Player) {
+
+    }
+
+    private actuate() {
+        switch(this.decision) {
+            case 'accelerate':
+                this.player.accelerate();
+                break;
+
+            case 'decelerate':
+                this.player.decelerate();
+                break;
+
+            case 'rotateLeft':
+                this.player.rotateLeft();
+                break;
+
+            case 'rotateRight':
+                this.player.rotateRight();
+                break;
+        }
+    }
+
+    public decide() {
+        this.decision = this.possibleDecisions[Math.floor(Math.random() * this.possibleDecisions.length)];
+        this.actuate();
+    }
+}
+
+export = Actuator;
--- src/public/js/game/Game.ts
+++ src/public/js/game/Game.ts
@@ -1,5 +1,6 @@
 import Field = require('game/Field');
 import Player = require('game/Player');
+import Actuator = require('game/Actuator');
 import Ball = require('game/Ball');
 import Coordinate = require('game/Coordinate');
 
@@ -7,6 +8,7 @@ class Game {
     private roundLength:number = 100;
     private field:Field;
     private players:Player[];
+    private actuators:Actuator[];
     private ball:Ball;
 
     private roundNumber:number = 100;
@@ -18,6 +20,12 @@ class Game {
             new Player(new Coordinate(this.field.width / 4, this.field.height / 2), 0, 'Bob'),
             new Player(new Coordinate(3 * this.field.width / 4, this.field.height / 2), Math.PI, 'Bobek')
         ];
+
+        // At first always the second player is controlled by AI.
+        this.actuators = [
+            new Actuator(this.players[1])
+        ];
+
         this.ball = new Ball(new Coordinate(this.field.width / 2, this.field.height / 2), 0);
 
         this.canvas.width = this.field.width;
@@ -31,7 +39,10 @@ class Game {
 
     private playRound(round:number):void {
         console.info('play round #' + round);
+
         this.players[0].move();
+        // At first always the second player is controlled by AI.
+        this.actuators[0].decide();
         this.players[1].move();
         this.ball.move();
 
