76173993af465d33808bd5190a1243efeb96e3ff
Gabriel Mičko <gabrielmicko@Gabriels-MacBook-Pro.local>
2016-04-08 22:52:46 +0200

    Routing, folders

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.use('/static', express.static('src/public/'));
+
+
+app.get('*', function(req, resp) {
+	resp.sendFile(__dirname + '/src/view/index.html');
 });
 
 app.listen(8888, function () {
