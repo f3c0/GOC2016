970044b54b556b22a5439ec5db87386a79a6cb13
user <user>
2016-04-08 21:32:34 +0200

    models

---
+++ models.js
@@ -0,0 +1,12 @@
+// global namespace
+var robotFootball = robotFootball || {};
+
+robotFootball.field = function()
+{
+
+}
+
+robotFootball.field.prototype.height = 90;
+robotFootball.field.prototype.width  = 160;
+
+console.log(robotFootball);
