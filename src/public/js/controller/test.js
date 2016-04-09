46a600d2932fa1192b623e2412260213386bc8fd
Gabriel Mičko <gabrielmicko@Gabriels-MacBook-Pro.local>
2016-04-09 04:31:52 +0200

    Data channel

--- src/public/js/controller/test.js
+++ src/public/js/controller/test.js
@@ -1,13 +1,75 @@
-define(['jquery', 'communication/socket'], function($, SocketHandler) {
+define(['jquery', 'communication/socket', 'communication/peerconnection'], function($, SocketHandler, PC) {
 	var socketHandler = new SocketHandler();
 	var connectionPromise = socketHandler.connect();
+	var peerConnection = new PC();
+
+	function messageManager(message) {
+		console.log(message);
+		switch(message.type) {
+			case 'connect':
+				peerConnection.init();
+				peerConnection.setListeners(
+					function() {
+					//ONOPEN
+					},
+					function() {
+					//ONCLOSE
+					},
+					function() {
+					//ONMSG
+					},
+					function() {
+					//ONCandidate
+					console.log('Jött candidate!');
+						socketHandler.sendMessage({
+							 'type': 'candidate',
+							 'room': config.roomName,
+							 'candidate': message.candidate
+						});
+					}
+				);
+				if(message.start == true) {
+					console.log('Crate offer!');
+					peerConnection.createOffer(function(offer) {
+						console.log('Sending offer:' + offer);
+						socketHandler.sendMessage({
+						    'type': 'offer',
+						    'room': config.roomName,
+							 'offer': offer
+						});
+					});
+				}
+				else {
+
+				}
+			break;
+			case 'offer':
+				console.log('Create answer!');
+				peerConnection.createAnswer(message.offer, function(answer) {
+					console.log('Sending answer:' + answer)
+					socketHandler.sendMessage({
+						 'type': 'answer',
+						 'room': config.roomName,
+						 'answer': answer
+					});
+				});
+			break;
+			case 'answer':
+				console.log('Answer received');
+				peerConnection.setLocalDescription(message.answer);
+			break;
+		}
+	}
+
 	connectionPromise.then(function() {
-
-
-
-		console.log('Kapcsolódott');
+		socketHandler.messageCallback = messageManager;
+		socketHandler.messageListener();
+		socketHandler.sendMessage({
+		    'type': 'connect',
+		    'room': config.roomName
+		});
 	}, function() {
-		console.log('Nem kapcsolodott');
+		console.log('Cannot connect to signalling server.');
 	});
 
 });
