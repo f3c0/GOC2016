ebf1b4ca3f92c6deb9dc9844697409c6dd8b11c4
feco <feco.boroczki@gmail.com>
2016-04-09 02:45:51 +0200

    delete map

--- src/public/js/game/GameObject.ts
+++ src/public/js/game/GameObject.ts
@@ -1,35 +1,29 @@
 import Coordinate = require('./Coordinate');
 
-class GameObject
-{
+class GameObject {
     private maxSpeed = 5;
     private minSpeed = 0;
 
     public speed:number = 0;
     public acceleration:number = 0;
 
-    constructor(public coordinate:Coordinate, public direction:number)
-    {
+    constructor(public coordinate:Coordinate, public direction:number) {
 
     }
 
-    public move()
-    {
+    public move() {
         this.coordinate.x += this.speed * Math.cos(this.direction);
         this.coordinate.y += this.speed * Math.sin(this.direction);
 
         this.calculateActualSpeed();
     }
 
-    private calculateActualSpeed()
-    {
-        this.speed        += this.acceleration;
-        if (this.speed > this.maxSpeed)
-        {
+    private calculateActualSpeed() {
+        this.speed += this.acceleration;
+        if (this.speed > this.maxSpeed) {
             this.speed = this.maxSpeed;
         }
-        else if (this.speed < this.minSpeed)
-        {
+        else if (this.speed < this.minSpeed) {
             this.speed = this.minSpeed;
         }
     }
