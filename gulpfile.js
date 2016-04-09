c48b9572f1bc361cb2396415487de05f6a39534b
feco <feco.boroczki@gmail.com>
2016-04-09 06:52:22 +0200

    try gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,15 @@
 var gulp = require('gulp');
-//var tsc = require('gulp-tsc');
-var tsc  = require('gulp-typescript-compiler');
+var tsc = require('gulp-tsc');
+//var tsc  = require('gulp-typescript-compiler');
 
 gulp.task('default', function () {
 	return gulp
-		.src('app/**/*.ts')
+		.src('src/**/*.ts')
 		.pipe(tsc({
 			module: 'amd',
 			target: 'ES5',
 			sourcemap: true,
 			logErrors: true
 		}))
-		.pipe(gulp.dest('dist'));
+		.pipe(gulp.dest('.'));
 });
